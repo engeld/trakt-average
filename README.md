6ac7629282468087adf48564d1a1358880e9e1d6
Dana Engel <engel.dana.mail@gmail.com>
2020-05-23 14:48:00 +0200

    code cleanup + new functionality: fetch rating of shows/seasons
    I've dusted a few things and split the whole big method of fetching the
    episode-ratings and calculating/printing into smaller, different
    methods. And I added fetching the rating of shows/season.
    
    I also tried to add jar-support but this worked only partially because
    of the setting-file I use - so this won't be in this commit (only in the
    README).

--- README.md
+++ README.md
@@ -15,6 +15,14 @@ Because my java was getting rusty and I wanted to brush it up a little bit. Mayb
 
 ### Use precompiled binary
 
-Download 
+Download the jar (here) and run:
+`java -jar traktaverage-0.0.0-alpha.jar `
+
+`java -jar target/traktaverage-0.0.0-alpha.jar -Dapp.properties="/Users/engeld/Dev/Workspaces/Eclipse/traktaverage/target/app.properties"`
 
 ### Use the source, Luke
+
+Download the source (here) and run:
+`java cc.engeld.traktaverage.App`
+
+`/Library/Java/JavaVirtualMachines/adoptopenjdk-14.0.1.jdk/Contents/Home/bin/java -Dfile.encoding=UTF-8 -classpath /Users/engeld/Dev/Workspaces/Eclipse/traktaverage/target/classes:/Users/engeld/.m2/repository/com/uwetrottmann/trakt5/trakt-java/6.5.0/trakt-java-6.5.0.jar:/Users/engeld/.m2/repository/com/squareup/retrofit2/retrofit/2.6.4/retrofit-2.6.4.jar:/Users/engeld/.m2/repository/com/squareup/okhttp3/okhttp/3.12.0/okhttp-3.12.0.jar:/Users/engeld/.m2/repository/com/squareup/okio/okio/1.15.0/okio-1.15.0.jar:/Users/engeld/.m2/repository/com/squareup/retrofit2/converter-gson/2.6.4/converter-gson-2.6.4.jar:/Users/engeld/.m2/repository/com/google/code/gson/gson/2.8.5/gson-2.8.5.jar:/Users/engeld/.m2/repository/org/threeten/threetenbp/1.4.1/threetenbp-1.4.1.jar:/Users/engeld/.m2/repository/org/apache/logging/log4j/log4j-api/2.13.3/log4j-api-2.13.3.jar:/Users/engeld/.m2/repository/org/apache/logging/log4j/log4j-core/2.13.3/log4j-core-2.13.3.jar cc.engeld.traktaverage.App`
--- src/main/java/cc/engeld/traktaverage/App.java
+++ src/main/java/cc/engeld/traktaverage/App.java
@@ -1,5 +1,6 @@
 package cc.engeld.traktaverage;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
@@ -7,11 +8,8 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Properties;
 import java.util.Scanner;
@@ -23,20 +21,15 @@ import org.apache.logging.log4j.core.config.Configurator;
 
 import com.uwetrottmann.trakt5.TraktV2;
 import com.uwetrottmann.trakt5.entities.AccessToken;
-import com.uwetrottmann.trakt5.entities.BaseEpisode;
-import com.uwetrottmann.trakt5.entities.BaseSeason;
-import com.uwetrottmann.trakt5.entities.BaseShow;
 import com.uwetrottmann.trakt5.entities.DeviceCode;
 import com.uwetrottmann.trakt5.entities.Episode;
 import com.uwetrottmann.trakt5.entities.RatedEpisode;
+import com.uwetrottmann.trakt5.entities.RatedSeason;
 import com.uwetrottmann.trakt5.entities.RatedShow;
-import com.uwetrottmann.trakt5.entities.Season;
 import com.uwetrottmann.trakt5.entities.Show;
 import com.uwetrottmann.trakt5.entities.UserSlug;
 import com.uwetrottmann.trakt5.enums.Extended;
-import com.uwetrottmann.trakt5.enums.Rating;
 import com.uwetrottmann.trakt5.enums.RatingsFilter;
-import com.uwetrottmann.trakt5.services.Shows;
 import com.uwetrottmann.trakt5.services.Users;
 
 import retrofit2.Response;
@@ -45,19 +38,20 @@ import retrofit2.Response;
  * Hello world!
  *
  */
-public class App 
-{
-    private static final Logger LOG = LogManager.getLogger();
+public class App {
+	private static final Logger LOG = LogManager.getLogger();
 	private static Properties prop = new Properties();
 
-    private static TraktV2 trakt = null;
-    public static void setTrakt(TraktV2 trakt) {
+	private static TraktV2 trakt = null;
+
+	public static void setTrakt(TraktV2 trakt) {
 		App.trakt = trakt;
 	}
+
 	protected static TraktV2 getTrakt() {
-        return trakt;
-    }
-    
+		return trakt;
+	}
+
 	private static void loadProperties() {
 		LOG.trace("loadProperties(): enter");
 		try (InputStream input = new FileInputStream("app.properties")) {
@@ -67,170 +61,202 @@ public class App
 		}
 		LOG.trace("loadProperties(): leave");
 	}
-	
+
 	private static void loadTrakt() {
 		LOG.trace("loadTrakt(): enter");
 		setTrakt(trakt = new TraktV2(
-			prop.getProperty("CLIENT_ID"), 
-			prop.getProperty("CLIENT_SECRET"), 
-			"urn:ietf:wg:oauth:2.0:oob"));
+				prop.getProperty("CLIENT_ID"), 
+				prop.getProperty("CLIENT_SECRET"),
+				"urn:ietf:wg:oauth:2.0:oob"));
+		
 		LOG.trace("loadTrakt(): leave");
 	}
-	
+
 	public static void auth() {
-		if(prop.getProperty("DEVICE_CODE").isEmpty()) {
+		if (prop.getProperty("DEVICE_CODE").isEmpty()) {
 			Response<DeviceCode> codeResponse = null;
 			try {
 				codeResponse = getTrakt().generateDeviceCode();
 				DeviceCode deviceCode = codeResponse.body();
-		        System.out.println("Device Code: " + deviceCode.device_code);
-		        System.out.println("User Code: " + deviceCode.user_code);
-		        System.out.println("Enter the user code at the following URI: " + deviceCode.verification_url);
-		        
-		        System.out.println("Press \"ENTER\" when finished...");
-		  	   	Scanner scanner = new Scanner(System.in);
-		  	   	scanner.nextLine();
-		  	   
+				System.out.println("Device Code: " + deviceCode.device_code);
+				System.out.println("User Code: " + deviceCode.user_code);
+				System.out.println("Enter the user code at the following URI: " + deviceCode.verification_url);
+
+				System.out.println("Press \"ENTER\" when finished...");
+				Scanner scanner = new Scanner(System.in);
+				scanner.nextLine();
+				scanner.close();
+
 				prop.setProperty("DEVICE_CODE", deviceCode.device_code);
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
 		}
-        
-		if(prop.getProperty("ACCESS_TOKEN").isEmpty()) {
+
+		if (prop.getProperty("ACCESS_TOKEN").isEmpty()) {
 			try {
-				Response<AccessToken> response = getTrakt().exchangeDeviceCodeForAccessToken(prop.getProperty("DEVICE_CODE"));
-				
-				if(response.isSuccessful()) {
+				Response<AccessToken> response = getTrakt()
+						.exchangeDeviceCodeForAccessToken(prop.getProperty("DEVICE_CODE"));
+
+				if (response.isSuccessful()) {
 					AccessToken accessToken = response.body();
 					System.out.println("Token: " + accessToken.access_token + " created at " + accessToken.created_at);
-					prop.setProperty("ACCESS_TOKEN",accessToken.access_token);
-				}else {
-	    	        if (response.code() == 401) {
-	    	        	System.out.println("authorization required, supply a valid OAuth access token");
-	    	        } else {
-	    	        	System.out.println("the request failed for some other reason");
-	    	        	System.out.println(response.code());
-	    	        }
-	    	    }
+					prop.setProperty("ACCESS_TOKEN", accessToken.access_token);
+				} else {
+					if (response.code() == 401) {
+						System.out.println("authorization required, supply a valid OAuth access token");
+					} else {
+						System.out.println("the request failed for some other reason");
+						System.out.println(response.code());
+					}
+				}
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
 		}
-    }
-    
-    
-    public static void main( String[] args ) {
-        Configurator.setRootLevel(Level.WARN);
-    	LOG.trace("main(): enter");
-    	
-    	loadProperties();
-    	loadTrakt();
-    	auth();
+	}
 
-    	LOG.info("main(): accessToken = " + prop.getProperty("ACCESS_TOKEN"));
-    	trakt.accessToken(prop.getProperty("ACCESS_TOKEN"));
-    	Users users = trakt.users();
-    	
-    	getAllRatedEpisodesGroupedByShows(users);
-    	
-    	cleanupProperties();
-    	LOG.trace("main(): leave");
-    }
-	
-	private static void getAllRatedEpisodesGroupedByShows(Users users) {
+	private static HashMap<String, HashMap<Integer, List<RatedEpisode>>> getAllRatedEpisodesGroupedByShows(Users users) {
 		LOG.trace("getAllRatedEpisodesByShows(): enter");
 		try {
-    		Response<List<RatedEpisode>> response = users.ratingsEpisodes(UserSlug.ME, RatingsFilter.ALL, Extended.FULL).execute();
-   		
-    	    if (response.isSuccessful()) {
-    	        List<RatedEpisode> episodes = response.body();
-    	        
-    	        HashMap<String, HashMap<Integer, List<RatedEpisode>>> hashMap2 = new HashMap<String, HashMap<Integer, List<RatedEpisode>>>();
+			Response<List<RatedEpisode>> response = users.ratingsEpisodes(UserSlug.ME, RatingsFilter.ALL, Extended.FULL).execute();
 
-    	        for (RatedEpisode episode : episodes) {
-    	            Show tmpShow = episode.show;
-	        		Episode epi = episode.episode;
+			if (response.isSuccessful()) {
+				List<RatedEpisode> episodes = response.body();
+				HashMap<String, HashMap<Integer, List<RatedEpisode>>> ratedEpisodesGrouped = new HashMap<String, HashMap<Integer, List<RatedEpisode>>>();
 
-    	        	if (!hashMap2.containsKey(tmpShow.title)) { // show not yet existent
-    	        		HashMap<Integer, List<RatedEpisode>> tmpHashMap = new HashMap<Integer, List<RatedEpisode>>();
+				for (RatedEpisode episode : episodes) {
+					if (!ratedEpisodesGrouped.containsKey(episode.show.title)) { // show not yet existent
+						HashMap<Integer, List<RatedEpisode>> tmpHashMap = new HashMap<Integer, List<RatedEpisode>>();
 
-    	        	    List<RatedEpisode> list = new ArrayList<RatedEpisode>();
-    	        	    list.add(episode);
-    	        	    
-    	        	    tmpHashMap.put(epi.season, list);
-    	        	    hashMap2.put(tmpShow.title, tmpHashMap);
-    	        	} else { // shows already in map
-    	        	    HashMap<Integer, List<RatedEpisode>> tmpHashMap = hashMap2.get(tmpShow.title);
-    	        	    
-    	        	    if(!tmpHashMap.containsKey(epi.season)) { // show exists, but season doesnt
-    	        	    	List<RatedEpisode> list = new ArrayList<RatedEpisode>();
-        	        	    list.add(episode);
-        	        	    tmpHashMap.put(epi.season, list);
-        	        	    hashMap2.replace(tmpShow.title, tmpHashMap);
-    	        	    } else { // show exists, season exists
-    	    	            List<RatedEpisode> list = (List<RatedEpisode>) tmpHashMap.get(epi.season);
-    	    	            list.add(episode);
-    	    	            
-        	        	    tmpHashMap.replace(epi.season, list);
-        	        	    hashMap2.replace(tmpShow.title, tmpHashMap);
-    	        	    }
-    	        	    
-    	        	}
-    	        }
-    	        
-    	        for (Entry<String, HashMap<Integer, List<RatedEpisode>>> entry : hashMap2.entrySet()) {
-    	            String key = entry.getKey(); // tv show title
-    	            HashMap<Integer, List<RatedEpisode>> value = (HashMap<Integer, List<RatedEpisode>>) entry.getValue(); // map<season, ratedEpi>
-    	            
-		        	System.out.println("===========================================================================");
-		        	System.out.println("> Show: "+ entry.getKey());
-		        	System.out.println("---------------------------------------------------------------------------");
-		        	
-		        	int showAverageRating = 0;
-		        	for (Entry<Integer, List<RatedEpisode>> entry2 : value.entrySet()) {
-		        		Integer season = entry2.getKey();
-		        		List<RatedEpisode> seasonEpisodes = entry2.getValue();
-		        		System.out.println();
-		        		System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
-		        		System.out.println(">> Season " + season);
-		        		System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
-		        		
-		        		int seasonAverageRating = 0;
-		        		for (RatedEpisode episode : seasonEpisodes) {
-		        			seasonAverageRating += episode.rating.value;
-		    	            System.out.println("+ Episode '" + episode.episode.title + "' (S0"+ episode.episode.season + "E"+episode.episode.number +") was rated " + episode.rating + "/10");
+						List<RatedEpisode> list = new ArrayList<RatedEpisode>();
+						list.add(episode);
+
+						tmpHashMap.put(episode.episode.season, list);
+						ratedEpisodesGrouped.put(episode.show.title, tmpHashMap);
+					} else { // shows already in map
+						HashMap<Integer, List<RatedEpisode>> tmpHashMap = ratedEpisodesGrouped.get(episode.show.title);
+
+						if (!tmpHashMap.containsKey(episode.episode.season)) { // show exists, but season doesnt
+							List<RatedEpisode> list = new ArrayList<RatedEpisode>();
+							list.add(episode);
+							tmpHashMap.put(episode.episode.season, list);
+							ratedEpisodesGrouped.replace(episode.show.title, tmpHashMap);
+						} else { // show exists, season exists
+							List<RatedEpisode> list = (List<RatedEpisode>) tmpHashMap.get(episode.episode.season);
+							list.add(episode);
+
+							tmpHashMap.replace(episode.episode.season, list);
+							ratedEpisodesGrouped.replace(episode.show.title, tmpHashMap);
 						}
-		        		
-			        	System.out.println("");
-			        	int seasonAverage = (seasonAverageRating/(seasonEpisodes.size()));
-		        		System.out.println(">> Average season rating is: " + seasonAverage);
-		        		showAverageRating += seasonAverage;
-		        	}
-		        	System.out.println("");
-		        	int showAverage = (showAverageRating/(value.size()));
-	        		System.out.println("> Average show rating is: " + showAverage);
-		        	System.out.println("");
-    	        }
-    	        
-    	        
-    	        System.out.println("Rated " + episodes.size() + " Episodes from " + hashMap2.size() + " Shows so far!");
-    	    } else {
-    	        if (response.code() == 401) {
-    	        	LOG.error("authorization required, supply a valid OAuth access token");
-    	        } else {
-    	        	LOG.error("the request failed for some other reason with response-code" + response.code());
-    	        }
-    	    }
-    	} catch (Exception e) {
-    		LOG.error("getAllRatedEpisodesByShows(): houston, we have an exception: " + e.getMessage());
-    	}		
-		LOG.trace("getAllRatedEpisodesByShows(): leave");
+
+					}
+				}
+				System.out.println("Rated " + episodes.size() + " Episodes from " + ratedEpisodesGrouped.size() + " Shows so far!");
+				LOG.trace("getAllRatedEpisodesByShows(): leave");
+				return ratedEpisodesGrouped;
+
+			} else {
+				if (response.code() == 401) {
+					LOG.error("authorization required, supply a valid OAuth access token");
+					return null;
+				} else {
+					LOG.error("the request failed for some other reason with response-code" + response.code());
+					return null;
+				}
+			}
+		} catch (Exception e) {
+			LOG.error("getAllRatedEpisodesByShows(): houston, we have an exception: " + e.getMessage());
+			return null;
+		}
+	}
+
+	private static void printRatings(HashMap<String, HashMap<Integer, List<RatedEpisode>>> groupedEpisodes) {
+		for (Entry<String, HashMap<Integer, List<RatedEpisode>>> entry : groupedEpisodes.entrySet()) {
+			System.out.println("===========================================================================");
+			System.out.println("> Show: " + entry.getKey());
+			System.out.println("---------------------------------------------------------------------------");
+
+			int showAverageRating = 0;
+			HashMap<Integer, List<RatedEpisode>> value = (HashMap<Integer, List<RatedEpisode>>) entry.getValue(); // map<season, ratedEpi>
+			for (Entry<Integer, List<RatedEpisode>> entry2 : value.entrySet()) {
+				Integer season = entry2.getKey();
+				List<RatedEpisode> seasonEpisodes = entry2.getValue();
+				System.out.println();
+				System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+				System.out.println(">> Season " + season);
+				System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+
+				int seasonAverageRating = 0;
+				for (RatedEpisode episode : seasonEpisodes) {
+					seasonAverageRating += episode.rating.value;
+					System.out.println("+ Episode '" + episode.episode.title + "' (S0" + episode.episode.season + "E"
+							+ episode.episode.number + ") was rated " + episode.rating + "/10");
+				}
+
+				System.out.println("");
+				int seasonAverage = (seasonAverageRating / (seasonEpisodes.size()));
+				System.out.println(">> Average season rating is: " + seasonAverage);
+				showAverageRating += seasonAverage;
+			}
+			System.out.println("");
+			int showAverage = (showAverageRating / (value.size()));
+			System.out.println("> Average show rating is: " + showAverage);
+			System.out.println("");
+		}
 	}
 	
+	private static HashMap<String, RatedSeason> getAllRatedSeasonsByShows(Users users) {
+		LOG.trace("getAllRatedSeasonsByShows(): enter");
+		try {
+			Response<List<RatedSeason>> response = users.ratingsSeasons(UserSlug.ME, RatingsFilter.ALL, Extended.FULL).execute();
+
+			if (response.isSuccessful()) {
+				List<RatedSeason> seasons = response.body();
+				HashMap<String, RatedSeason> ratedSeasonGrouped = new HashMap<String, RatedSeason>();
+				
+				for (RatedSeason ratedSeason : seasons) {
+					System.out.println("Show " + ratedSeason.show.title + " - "+ ratedSeason.season.title + " was rated " + ratedSeason.rating + "/10");
+				}
+				
+
+				LOG.trace("getAllRatedSeasonsByShows(): leave");
+				return ratedSeasonGrouped;
+			}
+		} catch (Exception e) {
+			LOG.error("getAllRatedSeasonsByShows(): houston, we have an exception: " + e.getMessage());
+			return null;
+		}
+		return null;
+	}
+	
+	private static List<RatedShow> getAllRatedShows(Users users){
+		LOG.trace("getAllRatedShows(): enter");
+
+		try {
+			Response<List<RatedShow>> response = users.ratingsShows(UserSlug.ME, RatingsFilter.ALL, Extended.FULL).execute();
+
+			if (response.isSuccessful()) {
+				List<RatedShow> ratedShows = response.body();
+				for (RatedShow ratedShow : ratedShows) {
+					System.out.println("Show " + ratedShow.show.title + " was rated " + ratedShow.rating + "/10");
+				}
+
+				LOG.trace("getAllRatedShows(): leave");
+				return ratedShows;
+			}
+		} catch (Exception e) {
+			LOG.error("getAllRatedShows(): houston, we have an exception: " + e.getMessage());
+			return null;
+		}
+		return null;
+	}
+
 	private static void cleanupProperties() {
 		LOG.trace("cleanupProperties(): enter");
-		try (OutputStream output = new FileOutputStream("app.properties")){
+
+		// save external properties
+		try (OutputStream output = new FileOutputStream("app.properties")) {
 			prop.store(output, null);
 		} catch (FileNotFoundException e) {
 			LOG.error("cleanupProperties(): " + e.toString());
@@ -239,7 +265,31 @@ public class App
 		}
 		LOG.trace("cleanupProperties(): leave");
 	}
-	
+
+	public static void main(String[] args) {
+		Configurator.setRootLevel(Level.WARN);
+		LOG.trace("main(): enter");
+
+		loadProperties();
+		loadTrakt();
+		auth();
+
+		LOG.debug("main(): accessToken = " + prop.getProperty("ACCESS_TOKEN"));
+		trakt.accessToken(prop.getProperty("ACCESS_TOKEN"));
+		
+		// map<Show, map<Season, ratedEpi>>
+		HashMap<String, HashMap<Integer, List<RatedEpisode>>> ratedEpisodesGrouped = getAllRatedEpisodesGroupedByShows(trakt.users());
+		HashMap<String, RatedSeason> ratedSeasonGrouped = getAllRatedSeasonsByShows(trakt.users());
+		List<RatedShow> ratedShow = getAllRatedShows(trakt.users());
+		
+		// compare the rated seasons/shows to calulated averages rating
+//		if (ratedEpisodesGrouped != null) {
+//			printRatings(ratedEpisodesGrouped);
+//			compareRatings(episodeGrouped);
+//		}
+
+		cleanupProperties();
+		LOG.trace("main(): leave");
+	}
+
 }
-
-
